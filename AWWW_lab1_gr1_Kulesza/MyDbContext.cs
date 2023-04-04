f29eb631338854396532f7dea4499b9fb93646d5
croozer4 <konradkul12@gmail.com>
2023-04-04 09:13:35 +0200

    dodanie dbcontext

--- AWWW_lab1_gr1_Kulesza/MyDbContext.cs
+++ AWWW_lab1_gr1_Kulesza/MyDbContext.cs
@@ -1,7 +1,11 @@
-﻿namespace AWWW_lab1_gr1_Kulesza
-{
-    public class MyDbContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using AWWW_lab1_gr1_Kulesza;
 
+namespace AWWW_lab1_gr1_Kulesza
+{
+    public class MyDbContext: DbContext
+    {
+        public virtual DbSet<Article>? Articles { get; set; }
+        public virtual DbSet<Author>? Authors { get; set; }
     }
 }
--- AWWW_lab1_gr1_Kulesza/Program.cs
+++ AWWW_lab1_gr1_Kulesza/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using AWWW_lab1_gr1_Kulesza;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("MyConnection");
+builder.Services.AddDbContext<MyDbContext>(x => x.UseSqlServer(connectionString);
 
 var app = builder.Build();
 
