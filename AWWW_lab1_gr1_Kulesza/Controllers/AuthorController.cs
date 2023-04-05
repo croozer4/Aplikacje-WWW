4754d1e3b0020d2999a510a89d455adc82c83387
croozer4 <konradkul12@gmail.com>
2023-04-05 17:34:20 +0200

    dziala

---
+++ AWWW_lab1_gr1_Kulesza/Controllers/AuthorController.cs
@@ -0,0 +1,39 @@
+﻿using AWWW_lab1_gr1_Kulesza;
+using AWWW_lab1_gr1_Kulesza.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AWWW_lab1_gr1_Kulesza.Controllers
+{
+	public class AuthorController : Controller
+	{
+		private readonly MyDbContext _dbContext;
+
+		public AuthorController(MyDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public IActionResult Index(int id)
+		{
+			var author = _dbContext.Authors!.FirstOrDefault(a => a.Id == id); //Repository.Authors.ToList()[id];
+			return View(author);
+		}
+
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Add(string FirstName, string LastName)
+		{
+			Author author = new Author(FirstName, LastName);
+
+			_dbContext.Authors!.Add(author); //Repository.AddAuthor(author);
+			_dbContext.SaveChanges();
+
+			return View("Added", author);
+		}
+	}
+}
